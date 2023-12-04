1da975969b41dcbfc52eb3b3670ceeab20060618
x86skwizer <yassineamrire@protonmail.com>
2023-12-04 12:10:58 -0800

    init project

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Parent class (called BaseModel) to take care of instances's:
+-Initialization
+-Serialization
+-Deserialization
+"""
+
+
+class BaseModel():
+    """Class BaseModel define all common attributes/methods"""
+    pass
