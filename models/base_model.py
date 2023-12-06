346482bd12d5125a81644fc67c928446a83af26c
mberrouk <mberrouk@student.1337.ma>
2023-12-06 21:25:17 +0100

    storage_new

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
 
     def __str__(self):
