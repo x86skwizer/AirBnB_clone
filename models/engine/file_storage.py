5ce403ddd240069bc5a31f593c0250efd536cc1f
mberrouk <mberrouk@student.1337.ma>
2023-12-06 21:02:33 +0100

    __name__

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,7 @@ class FileStorage():
 
     def new(self, obj):
         """sets in __objects the obj with key"""
-        key = obj.__class__.name + "." + obj.id
+        key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
