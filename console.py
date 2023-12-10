5168267a84335f30514862e6a5a753837daa307e
mberrouk <mberrouk@student.1337.ma>
2023-12-10 22:39:41 +0100

    update EOF

--- console.py
+++ console.py
@@ -32,6 +32,7 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """EOF command to exit the program
         """
+        print("")
         return True
 
     def do_quit(self, line):
