8e0c8f2cd2c6a56859b487ef72c8650c0a15a6fa
x86skwizer <yassineamrire@protonmail.com>
2024-08-19 03:20:02 +0100

    classes that inherit from BaseModel

--- console.py
+++ console.py
@@ -1,114 +1,200 @@
 #!/usr/bin/python3
+"""
+Program called console.py that contains
+the entry point of the command interpreter
+"""
 import cmd, shlex
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
 from models import storage
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
+    """command interpreter's class HBNBCommand"""
+
     prompt = '(hbnb) '
-    
-    def do_quit(self, arg):
-        """Quit command to exit the program."""
+    class_mapping = {
+        "BaseModel": BaseModel,
+        "User": User,
+        'State': State,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "Review": Review
+    }
+
+    def do_EOF(self, line):
+        """EOF command to exit the program
+        """
+        print("")
         return True
 
-    def do_EOF(self, arg):
-        """Exit on EOF."""
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
         return True
 
-    def do_help(self, arg):
-        """Override help to include custom help."""
-        super().do_help(arg)
-        
-    def do_create(self, line):
-        """Creates a new instance of BaseModel, saves it and prints the id."""
+    def default(self, line):
+        """handle Class name input
+        """
+        class_mapping = {
+            "all": self.do_all,
+            "count": self.do_count,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update
+        }
+        args = line.strip()
+        args = args.split(".")
+        if len(args) != 2:
+            cmd.Cmd.default(self, line)
+            return
+        name_class = args[0]
+        if '(' in args[1]:
+            arg_parts = args[1].split('(')
+            command_parts = arg_parts[1].split(')')
+            command_p = command_parts[0].split(',')
+            str_com = " ".join(str(e) for e in command_p)
+            if len(command_parts) > 1:
+                command = [arg_parts[0], args[0], command_parts[0]]
+                if command[0] in class_mapping.keys():
+                    call = "{} {}".format(args[0], str_com)
+                    return class_mapping[command[0]](call)
+        print("*** Unknown syntax: {}".format(line))
+
+    def do_count(self, line):
+        """Count instanes of a class:
+        Usage: <class name>.count().
+        """
         if not line:
             print("** class name missing **")
-        elif line != "BaseModel":
+            return
+        args = shlex.split(line)
+        if args[0] not in self.class_mapping:
+            print("** class doesn't exist **")
+            return
+        store_dict = storage.all()
+        n = 0
+        for key, value in store_dict.items():
+            if value.to_dict()['__class__'] == args[0]:
+                n = n + 1
+        print(n)
+
+    def emptyline(self):
+        """Handle empty line case
+        """
+        pass
+
+    def do_create(self, line):
+        """Creat command function
+        """
+        if not line:
+            print("** class name missing **")
+            return
+        args = shlex.split(line)
+        if args[0] not in self.class_mapping:
             print("** class doesn't exist **")
         else:
-            new_model = BaseModel()
+            new_model = self.class_mapping[args[0]]()
             new_model.save()
             print(new_model.id)
-            
+
     def do_show(self, line):
-        """Prints the string representation of an instance based on the class name and id."""
+        """show command function
+        """
         if not line:
             print("** class name missing **")
             return
-        args = line.split()
-        if args[0] != "BaseModel":
+        args = shlex.split(line)
+        if args[0] not in self.class_mapping:
             print("** class doesn't exist **")
-            return  
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
         store_dict = storage.all()
         for key, value in store_dict.items():
-            if value.to_dict()['id'] == args[1]:
-                print(value)
-                return
+            if value.to_dict()['__class__'] == args[0]:
+                if value.to_dict()['id'] == args[1]:
+                    print(value)
+                    return
         print("** no instance found **")
-        
+
     def do_destroy(self, line):
+        """destroy command function
+        """
         if not line:
             print("** class name missing **")
             return
-        args = line.split()
-        if args[0] != "BaseModel":
+        args = shlex.split(line)
+        if args[0] not in self.class_mapping:
             print("** class doesn't exist **")
-            return  
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
         store_dict = storage.all()
         for key, value in store_dict.items():
-            if value.to_dict()['id'] == args[1]:
-                del store_dict[key]
-                storage.save()
-                storage.reload()
-                return
+            if value.to_dict()['__class__'] == args[0]:
+                if value.to_dict()['id'] == args[1]:
+                    del store_dict[key]
+                    storage.save()
+                    storage.reload()
+                    return
         print("** no instance found **")
-          
+
     def do_all(self, line):
+        """all command function
+        """
         if not line:
             print("** class name missing **")
-        elif line != "BaseModel":
+            return
+        args = shlex.split(line)
+        if args[0] not in self.class_mapping:
             print("** class doesn't exist **")
         else:
             lst = []
             store_dict = storage.all()
             for key, value in store_dict.items():
-                lst.append(str(value))
-            print(lst)
-            
+                if value.to_dict()['__class__'] == args[0]:
+                    lst.append(str(value))
+            if lst:
+                print(lst)
+
     def do_update(self, line):
+        """update command function
+        """
         if not line:
             print("** class name missing **")
             return
         args = shlex.split(line)
-        if args[0] != "BaseModel":
+        if args[0] not in self.class_mapping:
             print("** class doesn't exist **")
-            return  
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
         store_dict = storage.all()
         for key, value in store_dict.items():
-            if value.to_dict()['id'] == args[1]:
-                if len(args) < 3:
-                    print("** attribute name missing **")
+            if value.to_dict()['__class__'] == args[0]:
+                if value.to_dict()['id'] == args[1]:
+                    if len(args) < 3:
+                        print("** attribute name missing **")
+                        return
+                    if len(args) < 4:
+                        print("** value missing **")
+                        return
+                    value.__dict__[args[2]] = args[3]
+                    value.save()
                     return
-                if len(args) < 4:
-                    print("** value missing **")
-                    return
-                value.__dict__[args[2]] = args[3]
-                value.save()
-                return
         print("** no instance found **")
 
-    def emptyline(self):
-        """Override emptyline to do nothing."""
-        pass
 
 if __name__ == '__main__':
+    """Main function for the console"""
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,60 @@
 #!/usr/bin/python3
+"""
+File contains a class FileStorage that serializes instances
+to a JSON file and deserializes JSON file to instances
+"""
 import json, os
 
 
 class FileStorage():
-	__file_path = "file.json"
-	__objects = {}
+    """Class serialize instances to JSON file"""
 
-	def all(self):
-		"""Returns the dictionary __objects."""
-		return self.__objects
-	
-	def new(self, obj):
-		"""Sets in __objects the obj with key <obj class name>.id."""
-		key = f"{obj.__class__.__name__}.{obj.id}"
-		self.__objects[key] = obj
+    __file_path = "file.json"
+    __objects = {}
 
-	def save(self):
-		"""Serializes __objects to the JSON file (path: __file_path)."""
-		tmp = {}
-		with open( self.__file_path , "w" ) as write_file:
-			tmp = {key: value.to_dict() for key,value in self.__objects.items()}
-			json.dump(tmp, write_file)
-	
-	def reload(self):
-		"""Deserializes the JSON file to __objects, if it exists."""
-		try:
-			with open( self.__file_path , "r" ) as read_file:
-				from models.base_model import BaseModel
-				self.__objects = {key: BaseModel(**value) for key, value in json.load(read_file).items()}
-		except:
-			pass
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key"""
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        tmp = {}
+        with open(self.__file_path, "w", encoding="utf-8") as write_file:
+            tmp = {key: value.to_dict()
+                   for key, value in self.__objects.items()}
+            json.dump(tmp, write_file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+        try:
+            with open(self.__file_path, "r") as read_file:
+                class_mapping = {
+                    "BaseModel": BaseModel,
+                    "User": User,
+                    'State': State,
+                    "City": City,
+                    "Amenity": Amenity,
+                    "Place": Place,
+                    "Review": Review
+                }
+                if os.path.getsize(self.__file_path) == 0:
+                    self.__objects = {}
+                    return
+                json_data = json.load(read_file)
+                self.__objects = {key:
+                                  class_mapping[value['__class__']](**value)
+                                  for key, value in json_data.items()}
+        except (FileNotFoundError):
+            pass
