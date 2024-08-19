f3803191b23fc684ee79de4763185ccc8d4cb2ee
x86skwizer <yassineamrire@protonmail.com>
2024-08-19 01:10:36 +0100

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ Parent class (called BaseModel) to take care of instances's:
 """
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel():
@@ -28,6 +29,7 @@ class BaseModel():
 			self.id = str(uuid.uuid4())
 			self.created_at = datetime.now()
 			self.updated_at = self.created_at
+			storage.new(self)  # Add the new instance to storage
 
 	def __str__(self):
 		"""Return a string representation of the instance."""
@@ -36,6 +38,7 @@ class BaseModel():
 	def save(self):
 		"""Update the public instance attribute updated_at with the current datetime."""
 		self.updated_at = datetime.now()
+		storage.save()
 
 	def to_dict(self):
 		"""Return a dictionary containing all keys/values of __dict__ of the instance."""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
-
+import json, os
 
 
 class FileStorage():
 	__file_path = "file.json"
 	__objects = {}
 
+	def all(self):
+		"""Returns the dictionary __objects."""
+		return self.__objects
 	
+	def new(self, obj):
+		"""Sets in __objects the obj with key <obj class name>.id."""
+		key = f"{obj.__class__.__name__}.{obj.id}"
+		self.__objects[key] = obj
+
+	def save(self):
+		"""Serializes __objects to the JSON file (path: __file_path)."""
+		tmp = {}
+		with open( self.__file_path , "w" ) as write_file:
+			tmp = {key: value.to_dict() for key,value in self.__objects.items()}
+			json.dump(tmp, write_file)
+	
+	def reload(self):
+		"""Deserializes the JSON file to __objects, if it exists."""
+		try:
+			with open( self.__file_path , "r" ) as read_file:
+				from models.base_model import BaseModel
+				self.__objects = {key: BaseModel(**value) for key, value in json.load(read_file).items()}
+		except:
+			pass
